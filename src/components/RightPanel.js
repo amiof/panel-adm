a9b38e0f3945b1f181bb63ac648d1313fa6d62af
amiof <arm131313@gmail.com>
2022-11-13 14:55:04 +0330

    pointer

--- src/components/RightPanel.js
+++ src/components/RightPanel.js
@@ -4,11 +4,11 @@ const RightPanel = () => {
         <div className="bg-black flex flex-col w-1/5" >
             <div className="w-full shadow-2xl shadow-dark-500 rounded-xl min-h-screen mt-4 bg-gray-900"  >
                 
-                <ul className=" mt-16 divide-y divide-slate-700 text-white">
-                    <li className="p-5 ">داشتبورد</li>
-                    <li className="p-5">کاربران </li>
-                    <li className="p-5 ">مدیریت</li>
-                    <li className="p-5">دیگر</li>
+                <ul className=" mt-16 divide-y divide-slate-700 text-white cursor-pointer">
+                    <li className="p-5 hover:text-red-500">داشتبورد</li>
+                    <li className="p-5 hover:text-red-500">کاربران </li>
+                    <li className="p-5 hover:text-red-500 ">مدیریت</li>
+                    <li className="p-5 hover:text-red-500 ">دیگر</li>
                 </ul>
                 
             </div>
