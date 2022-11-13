622b88387ecf7edb462aef958a93f4a0ed9641da
amiof <arm131313@gmail.com>
2022-11-13 19:34:59 +0330

    cleaner svges

--- src/components/RightPanel.js
+++ src/components/RightPanel.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React from "react";
+import { dashbord, manage, other, users } from "../pic/svg";
 const RightPanel = () => {
-    return (
-        <div className="bg-black/95 flex flex-col w-1/5" >
-            <div className="w-full shadow-2xl shadow-dark-500 rounded-xl min-h-screen mt-4 bg-gray-900"  >
-                
-                <ul className=" mt-16 divide-y divide-slate-700 text-white/60  font-vazir font-bold ">
-                    <a href="./#" className="p-5 hover:text-white hover:bg-gray-800 flex justify-between align-middle group  ">داشتبورد<div className="group-hover:bg-red-600  w-1 inline-block"></div></a>
-                    <a href='./#' className="p-5 hover:text-white hover:bg-gray-800 flex justify-between align-middle group">کاربران<div className="group-hover:bg-red-600  w-1 inline-block"></div></a>
-                    <a href='./#' className="p-5 hover:text-white hover:bg-gray-800 flex justify-between align-middle group">مدیریت<div className="group-hover:bg-red-600  w-1 inline-block"></div></a>
-                    <a href='./#' className="p-5 hover:text-white hover:bg-gray-800 flex justify-between align-middle group">دیگر<div className="group-hover:bg-red-600  w-1 inline-block"></div></a>
-                
-                </ul>
-                
-            </div>
-        </div>
-    );
+  return (
+    <div className="bg-black/95 flex flex-col w-1/5">
+      <div className="w-full shadow-2xl shadow-dark-500 rounded-xl min-h-screen mt-4 bg-gray-900">
+        <ul className=" mt-16 divide-y divide-slate-700 text-white/60  font-vazir font-bold ">
+          <a
+            href="./#"
+            className="p-5 hover:text-white hover:bg-gray-800 flex justify-between align-middle group  ">
+            {dashbord}داشتبورد
+            <div className="group-hover:bg-red-600  w-1 inline-block"></div>
+          </a>
+          <a
+            href="./#"
+            className="p-5 hover:text-white hover:bg-gray-800 flex justify-between align-middle group">
+                 {users}کاربران
+            <div className="group-hover:bg-red-600  w-1 inline-block"></div>
+          </a>
+          <a
+            href="./#"
+            className="p-5 hover:text-white hover:bg-gray-800 flex justify-between align-middle group"
+          >{manage}مدیریت
+            <div className="group-hover:bg-red-600  w-1 inline-block"></div>
+          </a>
+          <a
+            href="./#"
+            className="p-5 hover:text-white hover:bg-gray-800 flex justify-between align-middle group"
+          >  {other}دیگر<div className="group-hover:bg-red-600  w-1 inline-block"></div>
+          </a>
+        </ul>
+      </div>
+    </div>
+  );
 };
 export default RightPanel;
---
+++ src/pic/svg.js
@@ -0,0 +1,82 @@
+const dashbord =  <svg
+xmlns="http://www.w3.org/2000/svg"
+class="icon icon-tabler icon-tabler-border-all"
+width="24"
+height="24"
+viewBox="0 0 24 24"
+stroke-width="2"
+stroke="currentColor"
+fill="none"
+stroke-linecap="round"
+stroke-linejoin="round"
+>
+<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+<rect x="4" y="4" width="16" height="16" rx="2"></rect>
+<line x1="4" y1="12" x2="20" y2="12"></line>
+<line x1="12" y1="4" x2="12" y2="20"></line>
+</svg>
+const users= <svg
+xmlns="http://www.w3.org/2000/svg"
+class="icon icon-tabler icon-tabler-users"
+width="24"
+height="24"
+viewBox="0 0 24 24"
+stroke-width="2"
+stroke="currentColor"
+fill="none"
+stroke-linecap="round"
+stroke-linejoin="round"
+>
+<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+<circle cx="9" cy="7" r="4"></circle>
+<path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path>
+<path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
+<path d="M21 21v-2a4 4 0 0 0 -3 -3.85"></path>
+</svg>
+const manage=  <svg
+xmlns="http://www.w3.org/2000/svg"
+class="icon icon-tabler icon-tabler-adjustments-horizontal"
+width="24"
+height="24"
+viewBox="0 0 24 24"
+stroke-width="2"
+stroke="currentColor"
+fill="none"
+stroke-linecap="round"
+stroke-linejoin="round"
+>
+<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+<circle cx="14" cy="6" r="2"></circle>
+<line x1="4" y1="6" x2="12" y2="6"></line>
+<line x1="16" y1="6" x2="20" y2="6"></line>
+<circle cx="8" cy="12" r="2"></circle>
+<line x1="4" y1="12" x2="6" y2="12"></line>
+<line x1="10" y1="12" x2="20" y2="12"></line>
+<circle cx="17" cy="18" r="2"></circle>
+<line x1="4" y1="18" x2="15" y2="18"></line>
+<line x1="19" y1="18" x2="20" y2="18"></line>
+</svg>
+
+const other =<svg
+xmlns="http://www.w3.org/2000/svg"
+class="icon icon-tabler icon-tabler-clipboard-list"
+width="24"
+height="24"
+viewBox="0 0 24 24"
+stroke-width="2"
+stroke="currentColor"
+fill="none"
+stroke-linecap="round"
+stroke-linejoin="round"
+>
+<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+<path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2"></path>
+<rect x="9" y="3" width="6" height="4" rx="2"></rect>
+<line x1="9" y1="12" x2="9.01" y2="12"></line>
+<line x1="13" y1="12" x2="15" y2="12"></line>
+<line x1="9" y1="16" x2="9.01" y2="16"></line>
+<line x1="13" y1="16" x2="15" y2="16"></line>
+</svg>
+
+
+export {dashbord, users, manage , other}
