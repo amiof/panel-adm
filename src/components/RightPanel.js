d5e90817ebf6ae5f50d3c87e0702d5e41fbc6e30
amiof <arm131313@gmail.com>
2022-11-14 18:17:41 +0330

    add scrule hidden in right-menu

--- src/components/RightPanel.js
+++ src/components/RightPanel.js
@@ -1,56 +1,84 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect } from "react";
 import { dashbord, manage, other, users } from "../pic/svg";
 const RightPanel = () => {
   // const [nav, setNav]=useState([])
-  useEffect(()=>{
-    getelement()
-  },[])
+  useEffect(() => {
+    getelement();
+  }, []);
 
-  const getelement =()=>{
-    const getEl= document.querySelectorAll(".nav-link")
-    console.log(getEl)
-    getEl.forEach(element => {
-      element.onclick= clickHandler
-      
+  const getelement = () => {
+    const getEl = document.querySelectorAll(".nav-link");
+    getEl.forEach((element) => {
+      element.onclick = (e) => {
+        e.preventDefault();
+        e.target.classList.toggle("visited");
+         const parent =e.target.parentNode.nextElementSibling;
+         parent && parent.classList.toggle("hid")
+      };
     });
-  }
+  };
 
-  
-  const clickHandler =(e)=>{
-    
-    e.preventDefault()
-    e.target.classList.toggle("visited")
-    //  console.log(e)
-    
-    
-  }
   return (
     <div className="bg-black/95 flex flex-col w-1/5">
       <div className="w-full shadow-2xl shadow-dark-500 rounded-xl min-h-screen mt-4 bg-gray-900">
         <ul className=" mt-16 divide-y divide-slate-700 text-white/60  font-vazir font-bold ">
-          <a
-            href="./df"
-            className="nav-link group  ">
-            {dashbord}داشتبورد
+          <li>
+            <div className="flex w-full justify-between rounded-3xl ">
+              <div className="group-hover:bg-red-600  w-1 inline-block"></div>
+              <a href="./df" className="nav-link group  ">
+                <p className="ml-5 mr-0">{dashbord}</p>
+                داشتبورد
+              </a>
+            </div>
+            <ul className="hidden font-vazir font-light relative  ">
+                <li className="sub-link">
+                  <a>مدیریت داشتبورد</a>
+                </li>
+                <li className="sub-link">
+                  <a>شخصی سازی</a>
+                </li>
+                <li className="sub-link">
+                  <a> لایسنس</a>
+                </li>
+              </ul>
+          </li>
+          
+            <li className="flex flex-col">
+              <div className="flex w-full justify-between rounded-3xl ">
+                <div className="group-hover:bg-red-600  w-1 inline-block"></div>
+                <a href="./#" className="nav-link ">
+                  <p className="ml-5 mr-0">{users}</p>
+                  کاربران
+                </a>
+              </div>
+              <ul className="hidden font-vazir font-light relative  ">
+                <li className="sub-link">
+                  <a>اضافه کردن کاربر جدید</a>
+                </li>
+                <li className="sub-link">
+                  <a>حذف کاربر</a>
+                </li>
+                <li className="sub-link">
+                  <a>ویرایش کاربر</a>
+                </li>
+              </ul>
+            </li>
+          
+          <div className="flex w-full justify-between rounded-3xl">
             <div className="group-hover:bg-red-600  w-1 inline-block"></div>
-          </a>
-          <a
-            href="./#"
-            className="nav-link group">
-                 {users}کاربران
+            <a href="./#" className="nav-link group">
+              <p className="ml-5 mr-0">{manage}</p>
+              مدیریت
+            </a>
+          </div>
+          <div className="flex w-full justify-between rounded-3xl group">
             <div className="group-hover:bg-red-600  w-1 inline-block"></div>
-          </a>
-          <a
-            href="./#"
-            className="nav-link group"
-          >{manage}مدیریت
-            <div className="group-hover:bg-red-600  w-1 inline-block"></div>
-          </a>
-          <a
-            href="./#"
-            className="nav-link group"
-          >  {other}دیگر<div className="group-hover:bg-red-600  w-1 inline-block"></div>
-          </a>
+            <a href="./#" className="nav-link group">
+              {" "}
+              <p className="ml-5 mr-0">{other}</p>
+              دیگر
+            </a>
+          </div>
         </ul>
       </div>
     </div>
